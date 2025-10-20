202b497e6afee72b1a5de5d53e8af1d0d1db147b
erroshan06 <rroshankum908@gmail.com>
2025-10-20 20:45:55 +0530

    commit

---
+++ JS/day_04/1_index.js
@@ -0,0 +1,29 @@
+//Number 
+
+// let a = 10 ;
+// let b = 345.5674567;
+//let c=b.toFixed(1)
+
+//console.log( typeof c);
+//console.log(b.toPrecision(4));
+// console.log(b.toString());
+
+
+//create number
+
+
+// let a = new Number(20);
+// let b = new  Number(20);
+// console.log(typeof a); //-: o/p object
+// console.log(a==b);
+
+
+// let obj1={
+//     name:"Roshan"
+// }
+// let obj2={
+//     name:"Roshan"
+// }
+// let obj3 = obj1;
+// console.log(obj1==obj3)//-: o/p- true
+// console.log(obj1==obj2) //-: o/p- false 
