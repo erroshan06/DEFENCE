136fab2b9ef8f3c39da51ddd447edccd065fb6e8
erroshan06 <rroshankum908@gmail.com>
2025-10-20 21:41:55 +0530

    commit

---
+++ JS/day_04/2_index.js
@@ -0,0 +1,30 @@
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.SQRT2);
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(6.3));
+// console.log(Math.log10(20));
+// console.log(Math.max(2,3,4,5,12));
+// console.log(Math.random());// it will genrate a random value b/w 0 to 1  , where  0  is included 
+
+
+// satebaaji ka game banata:(0-10)
+
+// console.log(Math.floor(Math.random()*10)+1);
+ 
+
+
+// generate (1-6)
+// console.log(Math.floor(Math.random()*6)+1);
+
+
+//generate 15 to 30
+console.log(Math.floor(Math.random()*11)+15);
+
+//Math.floor(Math.random *(max-min+1)+min)
+//Math.floor(Math.random()*(25-15+1)+15)
+
+//otp genrate :4 digits(1000-9999)
+
+console.log(Math.floor(Math.random() *(9999-1000+1)+1000))
