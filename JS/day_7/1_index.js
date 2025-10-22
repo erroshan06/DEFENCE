e20f59742088073fc0b8e8cd7806c210654d65bb
erroshan06 <rroshankum908@gmail.com>
2025-10-22 19:55:18 +0530

    COMMIT

---
+++ JS/day_7/1_index.js
@@ -0,0 +1,26 @@
+//Object
+// key value 
+
+const user ={
+    name:"Rohot",
+    age : 29,
+    emaiId : 'negi@gmail.com',
+    amount :4000
+}
+// console.log(user);
+// console.log(user.age);
+//console.log( typeof user);
+//CRUD operation : Create read update delete
+
+//insert ,update 
+user.adhar=1234;
+// console.log(user);
+
+
+user.amount=589887898;
+// console.log(user);
+
+
+//delete
+delete user.emaiId;
+console.log(user);
