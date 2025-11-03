7332a682c01d398fd3c79ce41d6e787b98ea349d
erroshan06 <rroshankum908@gmail.com>
2025-11-03 23:36:17 +0530

    commite

---
+++ JS/day_16/project_2/2_script.js
@@ -0,0 +1,7 @@
+const parent = document.getElementById('parent');
+const body= document.getElementById ('background');
+parent .addEventListener('click',(e)=>{
+    const child=e.target;
+  
+  body.style .backgroundColor= child.id;
+})
